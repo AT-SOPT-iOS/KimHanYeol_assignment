70c970126cd9c196cf4346a5586c6028b840c186
OneTen19 <khy000426@naver.com>
2025-04-16 01:55:57 +0900

    Initial Commit

---
+++ SOPT36_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT36_Assignment
+//
+//  Created by OneTen on 4/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
