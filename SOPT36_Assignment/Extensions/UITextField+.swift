d417874067d49d02ce04297030fdaf1bd77f11cb
OneTen19 <khy000426@naver.com>
2025-04-16 03:18:21 +0900

    [Feat] UITextField addLeftPadding extension 메소드 추가

--- SOPT36_Assignment/Extensions/UITextField+.swift
+++ SOPT36_Assignment/Extensions/UITextField+.swift
@@ -14,4 +14,10 @@ extension UITextField {
         }
         attributedPlaceholder = NSAttributedString(string: string, attributes: [.foregroundColor: color])
     }
+    
+    func addLeftPadding(width: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
 }
